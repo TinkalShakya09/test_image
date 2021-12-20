4794793864fa2eda3b936cfad1fef44a05d764ac
Tinkal Shakya <62926686+TinkalShakya09@users.noreply.github.com>
2021-12-20 13:33:03 +0530

    Third

--- index.php
+++ index.php
@@ -2,10 +2,12 @@
 <html>
 <body>
 
-<h1>My first PHP page</h1>
+<h1>My testing page One</h1>
+ <h2>My testing page Two</h2> 
 
 <?php
-echo "Hello World!";
+echo "Hello World! \n";
+  echo "I am Tinkal";
 ?> 
 
 </body>
